9099cb4f018a3a64f0437e6a4c261cd942f3a7bb
Michael Caputo <mcaputo@virmedica.com>
2016-11-15 19:04:57 -0500

    Read png in and print to text

---
+++ pixel_test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var fs = require('fs');
+var PNG = require('pngjs').PNG;
+var ctx = require('axel');
+var DEFAULT_STRING = 'abcdefghijklmnopqrstuvwxyz';
+
+ctx.clear();
+
+var imageUrl = process.argv[2];
+
+fs.createReadStream(imageUrl).pipe(new PNG({
+  filterType: 4
+})).on('parsed', function(data) {
+  var srcStrIdx = 0;
+  var sourceStr = process.argv[3] || DEFAULT_STRING;
+  console.log('source string', sourceStr)
+  for (var y = 0; y < this.width; y++) {
+    for (var x = 0; x < this.height; x++) {
+      var idx = (this.width * x + y) << 2;
+      var srcChar = sourceStr[srcStrIdx];
+
+      // Set pixel background color
+      ctx.fg(this.data[idx], this.data[idx + 1], this.data[idx + 2]);
+
+      ctx.text(y, x, srcChar);
+
+      srcStrIdx++;
+      if (srcStrIdx >= sourceStr.length) {
+        srcStrIdx = 0;
+      }
+
+      // invert the colors
+      // this.data[idx] = 255 - this.data[idx];
+      // this.data[idx + 1] = 255 - this.data[idx + 1];
+      // this.data[idx + 2] = 255 - this.data[idx + 2];
+    }
+  }
+  ctx.cursor.restore();
+//  this.pack().pipe(fs.createWriteStream('out.png'));
+}).on('error', function(err) {
+  console.log('err:',err);
+});
